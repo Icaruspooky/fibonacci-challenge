c5ea4361c72687f7fe6c58005c95f0857f9ef6c2
Icaro Cristofolini <icaroaj@hotmail.com>
2019-07-28 20:55:58 -0300

    Package placement correction.

--- src/main/java/br/com/fibonacci/challenge/Fibonacci.java
+++ src/main/java/br/com/fibonacci/challenge/Fibonacci.java
@@ -1,3 +1,5 @@
+package main.java.br.com.fibonacci.challenge;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/br/com/fibonacci/challenge/Main.java
+++ src/main/java/br/com/fibonacci/challenge/Main.java
@@ -1,3 +1,7 @@
+package main.java.br.com.fibonacci.challenge;
+
+import main.java.br.com.fibonacci.challenge.Fibonacci;
+
 public class Main {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci(350);
