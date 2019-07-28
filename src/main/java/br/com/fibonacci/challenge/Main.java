2b773b50981576d3e44d6ce74d126635191c1e38
Icaro Cristofolini <icaroaj@hotmail.com>
2019-07-28 20:57:26 -0300

    Package placement correction.

--- src/main/java/br/com/fibonacci/challenge/Main.java
+++ src/main/java/br/com/fibonacci/challenge/Main.java
@@ -1,7 +1,5 @@
 package main.java.br.com.fibonacci.challenge;
 
-import main.java.br.com.fibonacci.challenge.Fibonacci;
-
 public class Main {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci(350);
